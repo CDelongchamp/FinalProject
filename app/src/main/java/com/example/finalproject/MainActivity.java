93983d21f45649b51b41abf734443dbbc561d981
sammulvey <91491601+sammulvey@users.noreply.github.com>
2021-10-18 15:01:14 -0400

    created first part of UI, made new DB class
    created first part of UI, made new DB class

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username,password;
+    Button SignUpButton;
+    Button LogInButton;
+    DB_Management myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+
+        SignUpButton = (Button) findViewById(R.id.SignUpButton);
+        LogInButton = (Button)findViewById(R.id.LogInButton);
+
+        myDB = new DB_Management( this);
+
+        SignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("") || pass.equals(null)) {
+                    Toast.makeText(MainActivity.this, "Username or Password cannot be blank.", Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
+
+            }
+        });
+
     }
 }
