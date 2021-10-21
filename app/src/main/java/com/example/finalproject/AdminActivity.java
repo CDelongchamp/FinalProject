b70e0ed4c1c1235656c0a724da4f39ad31f58406
lprov <lprov055@uottawa.ca>
2021-10-21 12:40:14 -0400

    added AddUser, EditUser, RemoveUser java and pages

--- app/src/main/java/com/example/finalproject/AdminActivity.java
+++ app/src/main/java/com/example/finalproject/AdminActivity.java
@@ -2,16 +2,49 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminActivity extends AppCompatActivity {
+
+    Button addUserButton;
+    Button removeUserButton;
+    Button editUserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        addUserButton = findViewById(R.id.addUserButton);
+        removeUserButton = findViewById(R.id.removeUserButton);
+        editUserButton = findViewById(R.id.editUserButton);
+
+        addUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),AddUserActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        removeUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),RemoveUserActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        editUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),EditUserActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
 
 }
