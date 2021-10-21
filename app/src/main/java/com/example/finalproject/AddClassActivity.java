01ce62af237ab546218629b03cb8da92e6ef333f
sammulvey <91491601+sammulvey@users.noreply.github.com>
2021-10-21 13:42:41 -0400

    made add, remove, edit classActivity

---
+++ app/src/main/java/com/example/finalproject/AddClassActivity.java
@@ -0,0 +1,27 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AddClassActivity extends AppCompatActivity {
+
+    Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_class);
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
