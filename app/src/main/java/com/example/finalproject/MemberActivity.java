5a03804d7ddd3d8a0c57fc53d26f4dff784a117d
CDelongchamp <52896126+CDelongchamp@users.noreply.github.com>
2021-11-22 12:04:45 -0500

    UI fixes, added acitivities for deliverable 3

--- app/src/main/java/com/example/finalproject/MemberActivity.java
+++ app/src/main/java/com/example/finalproject/MemberActivity.java
@@ -3,10 +3,11 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MemberActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class MemberActivity extends AppCompatActivity {
     String username;
 //    String[] role;
     String role;
+    Button viewCurrentClassesButton , searchAllClassesButton, editClassButton, instructorDeleteClassButton;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -23,6 +25,8 @@ public class MemberActivity extends AppCompatActivity {
 
         myDB = new DB_Management(this );
         username = LoginActivity.getUser();
+        viewCurrentClassesButton = findViewById(R.id.viewCurrentClassesButton);
+        searchAllClassesButton = findViewById(R.id.SearchAllClassesButton);
 //        role = myDB.getUserRoles(username);
 
         if (HomeActivity.getRole()) {
@@ -35,6 +39,22 @@ public class MemberActivity extends AppCompatActivity {
         textView.setText("Welcome " + username + "! You are logged in as " + role + ".");
 
 //        Toast.makeText(MemberActivity.this, role[0], Toast.LENGTH_SHORT).show();
+
+        viewCurrentClassesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MemberViewClassesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        searchAllClassesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MemberSearchClassesActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
